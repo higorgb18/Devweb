75a50e380503f1945eaf502584762181c82a54c1
Higor <higorb2000@gmail.com>
2024-08-18 21:23:03 -0300

    [TR03] - Adicionando estrutura inicial do projeto

---
+++ src/test/java/com/iff/dev_web/DevWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iff.dev_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
